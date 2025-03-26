fef604b367085c82b12b8718ffc0bc75248edc43
JuanF88 <juanfhurtado@unicauca.edu.co>
2025-03-26 09:27:42 -0500

    ThreeDView update

--- src/pages/ThreeDView.js
+++ src/pages/ThreeDView.js
@@ -1,5 +1,7 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import LibroInteractivo from "@/components/libro";
@@ -7,11 +9,27 @@ import LibroInteractivo from "@/components/libro";
 export default function ThreeDView() {
   const [showIframe, setShowIframe] = useState(false);
 
+  const sectionRef = useRef(null);
+  const titleRef = useRef(null);
+  const isInView = useInView(sectionRef, { once: false, amount: 0.3 });
+  const controls = useAnimation();
+  const isTitleInView = useInView(titleRef, { once: false, amount: 0.2 });
+
+
+    useEffect(() => {
+      if (isInView) {
+        controls.start("visible");
+        window.scrollTo({ top: sectionRef.current.offsetTop, behavior: "smooth" });
+      } else {
+        controls.start("hidden");
+      }
+    }, [isInView, controls]);
+
   return (
     <div className="bg-black text-white min-h-screen flex flex-col">
       <Header />
 
-      <main className="flex flex-col items-center justify-center flex-grow text-center px-6 py-20 relative">
+      <main className="flex flex-col items-center justify-center flex-grow text-center px-6 py-80 relative">
         {!showIframe && (
           <button
             onClick={() => setShowIframe(true)}
@@ -37,8 +55,26 @@ export default function ThreeDView() {
             ></iframe>
           </div>
         )}
-              <LibroInteractivo/>
         
+
+
+
+
+ <section ref={sectionRef} className="min-h-screen flex items-center justify-center w-full relative z-20">
+          <div className="flex flex-wrap justify-center items-center gap-10 px-6 max-w-7xl relative z-20">
+          <LibroInteractivo/>
+
+
+          </div>
+        </section>
+
+
+
+
+
+
+
+
       </main>
 
       <Footer />
